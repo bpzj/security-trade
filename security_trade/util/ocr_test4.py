7c292f8637e6598e6da6cbd3fc7fa6074936018d
bpzj <1521189886@qq.com>
2020-06-09 21:00:42 +0800

    先把图像切割成4个单独的数字图像

--- security_trade/util/ocr_test4.py
+++ security_trade/util/ocr_test4.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 # 模式 "L" 为灰度图像，每个像素用8个bit表示，0表示黑，255表示白，其他数字表示不同的灰度。
 # 模式 "1" 为二值图像，每个像素用 8bit表示，0表示黑，255表示白 (没有其他值)。
-img = Image.open('screen.bmp').convert(mode='L').resize((62*2, 23*2))
+img = Image.open('screen.bmp').convert(mode='L').resize((62 * 2, 23 * 2))
 
 img.save("test1.jpg")
 # 获得文字图片的每个像素点
@@ -15,50 +15,50 @@ src_strlist = img.load()
 #  (0,2)
 #
 
-for j in range(img.height-1):
-    for i in range(img.width-1):
+for j in range(img.height - 1):
+    for i in range(img.width - 1):
         data = src_strlist[i, j]
-        if  data >= 150:
+        if data >= 150:
             print(0, end='')
         else:
             print(1, end='')
     print('')
-pre_is_split = False
-pre_split_x_value = []
-num_length_total = 0
-one_split_group = []
-for i in range(img.width-1):
-    total = 0
-    for j in range(img.height-1):
-        data = src_strlist[i, j]
-        total = total + data
-        # if  data >= 150:
-        #     print(0, end='')
-        # else:
-        #     print(1, end='')
-    if total >= 255 * (img.height - 1):
-        print("|",end='')
-        one_split_group.append(i)
-        pre_is_split = True
-    else:
-        if pre_is_split:
-            pre_split_x_value.append(sum(one_split_group)/len(one_split_group))
-            one_split_group = []
-            pre_is_split = False
-        num_length_total = num_length_total +1
-        print(" ",end='')
-    # print(sum,end='')
 
-print("每个数字平均长度为", num_length_total/4)
-cursor = 0
-real_split_x_value = []
-for i in pre_split_x_value:
-    if  i - cursor > num_length_total /4:
-        real_split_x_value.append(int(i))
-        cursor = i
 
-print(real_split_x_value)
+def get_split_position(image: Image, content_num=4) -> list:
+    pre_is_split = False
+    pre_split_x_value = []
+    content_length_total = 0
+    one_split_group = []
+    for i in range(image.width - 1):
+        total = 0
+        for j in range(image.height - 1):
+            total = total + src_strlist[i, j]
+        #  如果某一列的像素的灰度值基本都是白色, 就认为这一列没有内容, 作为分割线
+        if total >= 255 * (image.height - 1):
+            one_split_group.append(i)
+            pre_is_split = True
+        else:
+            content_length_total = content_length_total + 1
+            if pre_is_split:
+                # 当前列像素有内容, 前面的像素列是分割符, 这时候, 说明前面的可以分到一组里
+                pre_split_x_value.append(sum(one_split_group) / len(one_split_group))
+                one_split_group = []
+                pre_is_split = False
 
+    print("每个数字平均长度为", content_length_total / content_num)
+    cursor = 0
+    real_split_x_value = []
+    for split_pos in pre_split_x_value:
+        #  分割中心位置 - 上一个分割中心位置 > 每个数字占据的平均长度
+        if split_pos - cursor > content_length_total / content_num:
+            real_split_x_value.append(int(split_pos))
+            cursor = i
+
+    return real_split_x_value
+
+
+print()
 
 # 自定义灰度界限，大于这个值为黑色，小于这个值为白色
 threshold = 160
