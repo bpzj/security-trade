995a32c336db7b69ef38000c6ba159f079f91065
bpzj <1521189886@qq.com>
2020-06-09 13:09:16 +0800

    海通证券获取句柄逻辑优化

--- security_trade/haitong_ths/hold.py
+++ security_trade/haitong_ths/hold.py
@@ -12,20 +12,28 @@ from security_trade.util.win32_util import pos_in_window_rect
 
 class HoldPanel:
     def __init__(self, trade_hwnd):
-        self.__parent_trade = trade_hwnd
+        self.__whole_trade_handle = trade_hwnd
+        self.__AfxMDIFrame_handle = self.__find_AfxMDIFrame_hwnd()
         self.__handle = None
         self.__hwnd_list = None
         # self.__edit_set = {}
         self.__data_grid_hwnd = None
         self.available_cash = None
 
+    def __find_AfxMDIFrame_hwnd(self):
+        li = []
+        win32gui.EnumChildWindows(self.__whole_trade_handle, lambda handle, param: param.append(handle), li)
+        for l in li:
+            if win32gui.GetClassName(l) == 'AfxMDIFrame42s':
+                return l
+
     def __enter_hold_panel(self):
         """
         向主句柄 发送 F4，调出 查询 - 资金股票 界面
         :return:
         """
-        win32gui.PostMessage(self.__parent_trade, win32con.WM_KEYDOWN, win32con.VK_F4, 0)
-        win32gui.PostMessage(self.__parent_trade, win32con.WM_KEYUP, win32con.VK_F4, 0)
+        win32gui.PostMessage(self.__whole_trade_handle, win32con.WM_KEYDOWN, win32con.VK_F4, 0)
+        win32gui.PostMessage(self.__whole_trade_handle, win32con.WM_KEYUP, win32con.VK_F4, 0)
 
     def __init_handle(self):
         # 发送 F4 , 调出界面
@@ -36,7 +44,7 @@ class HoldPanel:
         """
         if self.__handle:
             son_of_hold = []
-            win32gui.EnumChildWindows(self.__parent_trade, lambda handle, param: param.append(handle), son_of_hold)
+            win32gui.EnumChildWindows(self.__whole_trade_handle, lambda handle, param: param.append(handle), son_of_hold)
             if self.__handle in son_of_hold:
                 li = []
                 win32gui.EnumChildWindows(self.__handle, lambda handle, param: param.append(handle), li)
@@ -46,23 +54,25 @@ class HoldPanel:
 
         # 获取所有 dialog 子句柄
         def call_back(handle, hwnd_li):
-            if win32gui.GetClassName(handle) == 'AfxMDIFrame42s':
-                win32gui.EnumChildWindows(handle, lambda handle, param: param.append(handle), hwnd_li)
+            if win32gui.GetClassName(handle) == '#32770':
+                hwnd_li.append(handle)
+        li = []
+        win32gui.EnumChildWindows(self.__AfxMDIFrame_handle, call_back, li)
+        left, top, right, bottom = win32gui.GetWindowRect(self.__whole_trade_handle)
+        for i in li:
+            left1, top1, right1, bottom1 = win32gui.GetWindowRect(i)
+            if top1 - top == 75 and right - right1 == 5 and bottom - bottom1 == 24:
+                print("******************************************")
+                list2 = []
+                win32gui.EnumChildWindows(i, lambda handle, param: param.append(handle), list2)
+                for l2 in list2:
+                    print(win32gui.GetWindowText(l2))
 
+        # self.__handle = hwnd_l[0]
         # txt = win32gui.GetWindowText(handle)
         # if txt and txt == "资金余额":
         #     hwnd_list.append(win32gui.GetParent(handle))
         #     return
-
-        li = []
-        win32gui.EnumChildWindows(self.__parent_trade, call_back, li)
-        left, top, right, bottom = win32gui.GetWindowRect(self.__parent_trade)
-
-        for item in li:
-            left1, top1, right1, bottom1 = win32gui.GetWindowRect(item)
-            if win32gui.GetClassName(item)=="#32770" and top1 - top == 75 and right - right1 == 5 and bottom -bottom1 == 24:
-                self.__handle = item
-        # self.__handle = hwnd_l[0]
 
         # 调用
         self.__set_useful_handle()
@@ -85,7 +95,7 @@ class HoldPanel:
         # 将窗口调到前台，激活
         self.__init_handle()
         # 另起进程处理输入验证码问题
-        confirm_pro = Process(target=handle_verify, args=(self.__parent_trade, self.__handle, self.__data_grid_hwnd))
+        confirm_pro = Process(target=handle_verify, args=(self.__whole_trade_handle, self.__handle, self.__data_grid_hwnd))
         confirm_pro.start()
         # ctrl c
         win32api.keybd_event(win32con.VK_LCONTROL, 0, 0, 0)
@@ -166,4 +176,3 @@ def win_is_verify_code(hand):
 
 if __name__ == '__main__':
     print('h')
-
